4a482a2c13e10a6c60674b5b1f3f963bea1130fd
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2021-10-20 09:10:18 +0530

    ytsearchmeow.py
    Just removed some unnecessary meows

--- DaisyXMusic/modules/ytsearch.py
+++ DaisyXMusic/modules/ytsearch.py
@@ -39,15 +39,13 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Matte Senpai, i am searching....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"Title 📜 - {results[i]['title']}\n"
+            text += f"Duration ⏳ - {results[i]['duration']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
